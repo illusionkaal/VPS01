05f8a0dbd9ede3db55cb5bc8ee12b007d9bf36ce
Siam19395 <siamchowdhury88888@gmail.com>
2024-06-25 01:11:53 +0600

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,18 +2,16 @@
 
 set -e
 
-/ngrok tcp --authtoken ${AUTH_TOKEN} 22 &
+./localtonet --key ${AUTH_TOKEN} -ssh 22 &
 sleep 5
 
 # Debugging information
-echo "Checking ngrok status..."
-curl -s http://localhost:4040/api/tunnels || echo "Error fetching ngrok status. Check if ngrok is running."
+echo "Checking Localtonet status..."
+localtonet_status=$(curl -s http://localhost:4040/api/tunnels)
 
-ngrok_status=$(curl -s http://localhost:4040/api/tunnels)
-
-# Check if the ngrok response is empty
-if [ -z "$ngrok_status" ]; then
-    echo "Error: ngrok is not running or returned an empty response."
+# Check if the localtonet response is empty
+if [ -z "$localtonet_status" ]; then
+    echo "Error: Localtonet is not running or returned an empty response."
     exit 1
 fi
 
@@ -21,15 +19,15 @@ fi
 python3 -c "
 import sys, json
 try:
-    data = json.loads('$ngrok_status')
+    data = json.loads('$localtonet_status')
     if 'tunnels' in data and len(data['tunnels']) > 0:
         tunnel = data['tunnels'][0]
         public_url = tunnel['public_url'][6:].replace(':', ' -p ')
         print(f'SSH Info:\\n ssh root@{public_url}\\nROOT Password:${PASSWORD}')
     else:
-        print('Error: No tunnels found in ngrok response.')
+        print('Error: No tunnels found in Localtonet response.')
 except json.JSONDecodeError:
-    print('Error: Failed to decode JSON response from ngrok.')
+    print('Error: Failed to decode JSON response from Localtonet.')
 "
 
 echo '/usr/sbin/sshd -D'
